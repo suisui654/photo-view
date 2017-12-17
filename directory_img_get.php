e8785f24726612a87ee32bc542c28fe8eea2f6e2
suisui654 <suisui654@gmail.com>
2017-12-18 00:27:01 +0900

    画像ファイルのみを返すように修正

--- directory_img_get.php
+++ directory_img_get.php
@@ -1,17 +1,17 @@
 <?php
-
 // ディレクトリのパスを指定する
 $dir = "./img/" ;
-
 // 画像のパスを入れる配列
 $img_array = array();
-
 if( is_dir( $dir ) && $handle = opendir( $dir ) ) {
-
 	$path = "";
 	while( ($file = readdir($handle)) !== false ) {
+
 		$path = $dir . $file;
-		if( filetype( $path ) == "file" && $type = exif_imagetype($path) ) {
+		$path_info = pathinfo( $path );
+
+		//画像ファイルかチェックする
+		if( img_check($path_info['extension']) ) {
  			$img_array[] = $path;
 		}
 	}
@@ -19,9 +19,16 @@ if( is_dir( $dir ) && $handle = opendir( $dir ) ) {
 
 //json形式で出力
 header('Content-type: application/json');
-
 $response = array();
 $response['response_deta'] = $img_array;
+echo json_encode($response);
 
-echo json_encode($response) ;
-
+//画像チェック
+function img_check($extension){
+	switch ($extension) {
+	    case "jpg": return true;
+	    case "png": return true;
+	    case "gif": return true;
+	    default: return false;
+	}
+}
